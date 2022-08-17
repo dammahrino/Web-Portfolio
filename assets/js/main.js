4d0c6080359d6824b92d21e96b19b145a537d270
Daniel Cadena <dacadenadi@gmail.com>
2022-08-16 22:15:24 -0500

    Theme Switcher between light / dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -163,7 +163,7 @@ window.addEventListener('scroll', scrollHeader);
 function scrollUp() {
     const scrollUp = document.querySelector('#scroll-up');
 
-    if(this.scrollY >= 560)
+    if (this.scrollY >= 560)
         scrollUp.classList.add('show-scroll');
     else
         scrollUp.classList.remove('show-scroll');
@@ -171,4 +171,27 @@ function scrollUp() {
 
 window.addEventListener('scroll', scrollUp);
 
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.querySelector('#theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+// Storage of selected configuration
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
